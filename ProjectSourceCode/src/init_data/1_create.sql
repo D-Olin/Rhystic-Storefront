afcbb023b0bc8667458ee4d8107832951d8e291d
D-Olin <diol8539@colorado.edu>
2024-07-03 15:31:53 -0600

    all db errors finally finished up

--- ProjectSourceCode/src/init_data/1_create.sql
+++ ProjectSourceCode/src/init_data/1_create.sql
@@ -11,17 +11,18 @@ DROP TABLE IF EXISTS cardinfo CASCADE;
 CREATE TABLE IF NOT EXISTS cardinfo (
     card_id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(100) NOT NULL,
-    discription TEXT NOT NULL,
+    description TEXT NOT NULL,
     image_url VARCHAR(300) NOT NULL,
-    cost VARCHAR(10) NOT NULL,
-    rarity VARCHAR(100) NOT NULL CONSTRAINT limited_values CHECK (rarity in ('common', 'uncommon', 'rare', 'mythic_rare'))
+    cost VARCHAR(10),
+    rarity VARCHAR(100) NOT NULL CONSTRAINT limited_values CHECK (rarity in ('common', 'uncommon', 'rare', 'mythic'))
 );
 
 DROP TABLE IF EXISTS user_to_card CASCADE;
 CREATE TABLE IF NOT EXISTS user_to_card (
 user_id INT NOT NULL,
 card_id INT NOT NULL,
-card_count INT NOT NULL,
+owned_count INT NOT NULL,
+cart_count INT NOT NULL,
 FOREIGN KEY (user_id) REFERENCES userinfo (user_id) ON DELETE CASCADE,
 FOREIGN KEY (card_id) REFERENCES cardinfo (card_id) ON DELETE CASCADE
 
