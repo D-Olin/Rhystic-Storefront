3a2c10d76eada3b07abdf250e094dcf984a8bb18
D-Olin <diol8539@colorado.edu>
2024-07-03 15:34:53 -0600

    added money

--- ProjectSourceCode/src/init_data/1_create.sql
+++ ProjectSourceCode/src/init_data/1_create.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS userinfo (
     name VARCHAR(100) NOT NULL,
     username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL
+    password VARCHAR(100) NOT NULL,
+    money int NOT NULL DEFAULT 100
 );
 
 DROP TABLE IF EXISTS cardinfo CASCADE;
