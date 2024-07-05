47266f0818123a123b61fe46ca6fa5e2a5ea413c
D-Olin <diol8539@colorado.edu>
2024-07-05 05:56:42 -0600

    added basic profile route, still need to check for login. moved copyright blurb to footer. Added navbar dropdown option as profile. added user_profile.hbs but may get rid of it.

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -37,17 +37,17 @@ const hbs = handlebars.create({
   app.use('/', express.static(path.join(__dirname, 'resources')));
   
   // -------------------------------------  DB CONFIG AND CONNECT   ---------------------------------------
-/* const dbConfig = {
+  const dbConfig = {
     host: 'db',
     port: 5432,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   };
-  const db = pgp(dbConfig); */
+  const db = pgp(dbConfig); 
   
   // db test
-/*   db.connect()
+  db.connect()
     .then(obj => {
       // Can check the server version here (pg-promise v10.1.0+):
       console.log('Database connection successful');
@@ -55,7 +55,7 @@ const hbs = handlebars.create({
     })
     .catch(error => {
       console.log('ERROR', error.message || error);
-    }); */
+    }); 
 
 
 
@@ -64,6 +64,15 @@ const hbs = handlebars.create({
 
 app.get('/', (req, res) => {
   res.render('pages/home', {
+  });
+});
+
+
+// -------------------------------------  ROUTES for profile.hbs   ----------------------------------------------
+
+// need to add a check that the user is logged in 
+app.get('/profile', (req, res) => {
+  res.render('pages/profile', {
   });
 });
 
