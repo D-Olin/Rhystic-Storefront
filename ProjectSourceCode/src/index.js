e454b7187a1b4ee6219bb23796a5909d9903b927
Byron Liu <byli4778@colorado.edu>
2024-07-06 12:45:14 -0600

    store page up and running fetching from Scryfall API, add to cart button currently broken

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -37,25 +37,25 @@ const hbs = handlebars.create({
   app.use('/', express.static(path.join(__dirname, 'resources')));
   
   // -------------------------------------  DB CONFIG AND CONNECT   ---------------------------------------
-/* const dbConfig = {
+const dbConfig = {
     host: 'db',
     port: 5432,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   };
-  const db = pgp(dbConfig); */
+  const db = pgp(dbConfig);
   
   // db test
-/*   db.connect()
-    .then(obj => {
-      // Can check the server version here (pg-promise v10.1.0+):
-      console.log('Database connection successful');
-      obj.done(); // success, release the connection;
-    })
-    .catch(error => {
-      console.log('ERROR', error.message || error);
-    }); */
+db.connect()
+  .then(obj => {
+    // Can check the server version here (pg-promise v10.1.0+):
+    console.log('Database connection successful');
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR', error.message || error);
+  });
 
 
 
@@ -63,8 +63,22 @@ const hbs = handlebars.create({
 
 
 app.get('/', (req, res) => {
-  res.render('pages/home', {
-  });
+  res.render('pages/home');
+});
+
+
+// -------------------------------------  ROUTES for store.hbs   ----------------------------------------------
+
+
+app.get('/store/search', (req, res) => {
+  var search_query = req.query.q;
+  var api_call = 'https://api.scryfall.com/cards/search?q=' + search_query + '+unique:prints+(game:paper)';
+  console.log(api_call);
+
+  const scryfallRes = fetch(api_call, {method: "GET"})
+    .then((response) => response.json())
+    .then((json) => res.render('pages/store', {json, search_query}));
+
 });
 
 // -------------------------------------  START THE SERVER   ----------------------------------------------
