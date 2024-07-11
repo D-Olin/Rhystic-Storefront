54e6f1afafb24fca0442f9bc918c832284f34f38
D-Olin <diol8539@colorado.edu>
2024-07-11 00:31:44 -0600

    added changes discussed in the meeting

--- ProjectSourceCode/src/init_data/1_create.sql
+++ ProjectSourceCode/src/init_data/1_create.sql
@@ -5,36 +5,39 @@ CREATE TABLE IF NOT EXISTS userinfo (
     username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL,
-    money int NOT NULL DEFAULT 100
-    -- pfp_url text DEFAULT "/img/profile_circle_icon.png"
+    money int NOT NULL DEFAULT 100,
+    pfp_url text DEFAULT '/img/profile_circle_icon.png',
+    cart text[][] DEFAULT [] --json obj of trade ids [[id,count],[id,count]]
 );
 
 DROP TABLE IF EXISTS cardinfo CASCADE;
 CREATE TABLE IF NOT EXISTS cardinfo (
-    card_id SERIAL PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    image_url TEXT NOT NULL,
-    mana_cost TEXT,
-    price DECIMAL(10,2),
-    rarity VARCHAR(100) NOT NULL CONSTRAINT limited_values CHECK (rarity in ('common', 'uncommon', 'rare', 'mythic','special','bonus'))
+    card_id VARCHAR(36) PRIMARY KEY NOT NULL,
+    card_name TEXT NOT NULL,
+    price DECIMAL(10,2)
 );
 
 DROP TABLE IF EXISTS trade CASCADE;
 CREATE TABLE IF NOT EXISTS trade (
     trade_id SERIAL PRIMARY KEY NOT NULL,
+    card_id VARCHAR(36) NOT NULL,
     trade_quantity INT NOT NULL,
     trade_price DECIMAL(10,2)
 );
 
 DROP TABLE IF EXISTS user_to_card CASCADE;
 CREATE TABLE IF NOT EXISTS user_to_card (
-    user_id INT NOT NULL,
-    card_id INT NOT NULL,
-    trade_id INT, -- allow null, add a check for if not null in the trade id and if not null, then theyve initiated a trade
-    owned_count INT NOT NULL,
-    cart_count INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES userinfo (user_id) ON DELETE CASCADE,
-    FOREIGN KEY (card_id) REFERENCES cardinfo (card_id) ON DELETE CASCADE,
-    FOREIGN KEY (trade_id) REFERENCES trade (trade_id) ON DELETE CASCADE
+user_id INT NOT NULL,
+card_id VARCHAR(36) NOT NULL,
+owned_count INT NOT NULL
+FOREIGN KEY (user_id) REFERENCES userinfo (user_id) ON DELETE CASCADE,
+FOREIGN KEY (card_id) REFERENCES cardinfo (card_id) ON DELETE CASCADE
 );
+
+DROP TABLE IF EXISTS user_to_trade CASCADE;
+CREATE TABLE IF NOT EXISTS user_to_trade (
+    seller_id INT NOT NULL,
+    trade_id INT NOT NULL,
+    FOREIGN KEY (seller_id) REFERENCES userinfo (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (trade_id) REFERENCES trade (trade_id) ON DELETE CASCADE
+)
--- ProjectSourceCode/src/init_data/2_populate.sql
+++ ProjectSourceCode/src/init_data/2_populate.sql
@@ -1,7 +1,29 @@
 INSERT INTO userinfo
-    (name,username,email,password)
+    (name,username,email,password,pfp_url)
 VALUES
-    ('John Doe','jDoe','john.doe@email.com','$2b$10$pFDtDge86AvOetyoNf4teuKhJWj/nQgwLS1pt6ueAz.NUJOAKjmAi'),
-    ('Jane Doe','janeD','jane.doe@email.com','$2b$10$emTwuumINsc7cArdTIMPheKxueEdjOgyMAVOO32XBtlQvDHQvVHCS');
--- pass1234 for john
--- password for jane
+    ('John Doe','jDoe','john.doe@email.com','$2b$10$Tru9/A34miRCbbY8YsnXH.EoHnj65H//COw6dr5qMDCAWunralPWm','https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F68%2F0e%2F24%2F680e241336ae8d3a57a42f54b656e58f.jpg&f=1&nofb=1&ipt=189ee3c805d95be9cf92baaa023f3ab79e5a71a7a12b5091be3b8731696356c3&ipo=images'),
+    ('Jane Doe','janeD','jane.doe@email.com','password',NULL,[[1,1]]);
+
+
+INSERT INTO cardinfo
+    (card_id,card_name,price)
+VALUES
+    ('a8a64329-09fc-4e0d-b7d1-378635f2801a','Fury Sliver', 0.42),
+    ('00006596-1166-4a79-8443-ca9f82e6db4e','Kor Outfitter', 0.15);
+
+
+INSERT INTO user_to_card
+    (user_id,card_id,owned_count)
+VALUES
+    (1,'a8a64329-09fc-4e0d-b7d1-378635f2801a',3),
+    (1,'00006596-1166-4a79-8443-ca9f82e6db4e',4);
+
+INSERT INTO trade
+    (card_id,trade_quantity,trade_price)
+VALUES
+    ('00006596-1166-4a79-8443-ca9f82e6db4e',1,35);
+
+INSERT INTO user_to_trade
+    (seller_id,trade_id)
+VALUES 
+    (1,1)
